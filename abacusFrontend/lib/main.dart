46e9e1eae1ec3e5d61b9cf56b0fb22abd45a6cef
Theassa <97030738+Theassa@users.noreply.github.com>
2023-08-31 14:55:50 +1000

    create login page

--- abacusFrontend/lib/main.dart
+++ abacusFrontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:abacusfrontend/pages/loginScreen.dart';
 import 'package:abacusfrontend/pages/homeScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(home: LoginScreen());
+    // return MaterialApp(home: HomeScreen());
   }
 }
