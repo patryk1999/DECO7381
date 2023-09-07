fdd03166c6ae40be81ab8c9690c72d4760aa1bd3
Selma <selma.gudmundsen@gmail.com>
2023-09-07 10:33:32 +1000

    changed color for app bar

--- abacusFrontend/lib/main.dart
+++ abacusFrontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:abacusfrontend/theme/colors.dart';
 import 'package:abacusfrontend/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -64,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: CustomColors.green,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
