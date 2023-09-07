0f864b32010202089178118c93a07f9f617986d6
Patryk <patrykku@ntnu.no>
2023-09-07 13:16:41 +1000

    added api for fetching a users friends

--- abacusBackend/users/urls.py
+++ abacusBackend/users/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('test/', views.test),
-    path('makeFriend/',views.makeFriends)
-    
+    path('makeFriend/',views.makeFriends),
+    path('newUser/', views.makeUser),
+    path('getFriends/', views.getFriends)
 ]
