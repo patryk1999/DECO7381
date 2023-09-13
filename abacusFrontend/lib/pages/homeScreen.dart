5d81110fd3318e270b4ba4fae1a69274c6e9a73a
Elin Bartnes <elin.bartneshotmail.com>
2023-09-13 15:30:07 +1000

    Added lizard

--- abacusFrontend/lib/pages/homeScreen.dart
+++ abacusFrontend/lib/pages/homeScreen.dart
@@ -41,63 +41,87 @@ class AppBarExample extends StatelessWidget {
         firstButton: firstButton,
         secondButton: secondButton,
       ),
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            width: double.infinity, // Use maximum width
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF78BC3F),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        side: BorderSide(
-                          color: const Color(0xFF386641), // Set border color
-                        ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 13), // Increase button height
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Join Run',
-                      style:
-                          TextStyle(fontSize: 20, fontStyle: FontStyle.normal),
-                    ),
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 4.0),
+            child: Center(
+              child: Text(
+                "Hey friend, welcome back! Ready to run today?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF78BC3F), // Sporty green color
+                  letterSpacing: 1.2, // Increased spacing for sporty look
                 ),
-                SizedBox(width: 20), // Add spacing between buttons
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF78BC3F),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        side: BorderSide(
-                          color: const Color(0xFF386641), // Set border color
-                        ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 13), // Increase button height
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Create Run',
-                      style:
-                          TextStyle(fontSize: 20, fontStyle: FontStyle.normal),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          Image.asset('assets/lizard.webp', width: 180, height: 180),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  width: double.infinity, // Use maximum width
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF78BC3F),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              side: BorderSide(
+                                color:
+                                    const Color(0xFF386641), // Set border color
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 13), // Increase button height
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Join Run',
+                            style: TextStyle(
+                                fontSize: 20, fontStyle: FontStyle.normal),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20), // Add spacing between buttons
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF78BC3F),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              side: BorderSide(
+                                color:
+                                    const Color(0xFF386641), // Set border color
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 13), // Increase button height
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Create Run',
+                            style: TextStyle(
+                                fontSize: 20, fontStyle: FontStyle.normal),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
