09f17f24e3b1fb93c2a35c10e99259534119a879
Tord Gunnarsli <tordsg@stud.ntnu.no>
2023-10-11 16:18:51 +1000

    reverted abacusFrontend

--- abacusFrontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ abacusFrontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_webrtc
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
